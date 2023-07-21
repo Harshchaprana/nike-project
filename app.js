0a01dde59726722d85c6c903e038788b0baf6882
kamalbaisla <kamalbaisla74@gmail.com>
2023-07-22 00:35:39 +0530

    nike api

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+
+const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/Products")
+
+app.get("/", (req, res) => {
+  res.send("hi iam live raju bhai")
+});
+
+app.use("/api/products", products_routes )
+ 
+const Start = async () => {
+    try{
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(PORT , () => {
+         console.log(`${PORT} Yes I am connected raju bhai`)   
+        }) 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Start();

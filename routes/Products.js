bf7b360dedb8d2e412f58b2141feaa04de833228
Harshchaprana <123630469+Harshchaprana@users.noreply.github.com>
2023-07-22 17:21:40 +0530

    Update Products.js

--- routes/Products.js
+++ routes/Products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {
     getAllProducts
     ,getAllProductsTesting
-} = require("./controllers/products"); 
+} = require("../controllers/products"); 
 
 router.route("/").get(getAllProducts);
 router.route("/testing").get(getAllProductsTesting);

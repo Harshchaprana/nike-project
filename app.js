3a4a12104af8a6544e8e35b3c8f10f7f19c2bf25
Harshchaprana <123630469+Harshchaprana@users.noreply.github.com>
2023-07-23 17:36:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ app.get("/", (req, res) => {
   res.send("hi iam live raju bhai")
 });
 
-app.use(cors({
-  origin:"http://localhost:5173","/api/products", products_routes 
-}))
+app.use(cors({origin:"http://localhost:5173/"},"/api/products", products_routes) )
  
 const Start = async () => {
     try{

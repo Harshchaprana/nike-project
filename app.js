3466f0cfcc55c553c3904ee9e0279ead35ed762f
Harshchaprana <123630469+Harshchaprana@users.noreply.github.com>
2023-08-06 15:34:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
-const cors = require("cors");
+const cors = require("cors")
 const PORT = process.env.PORT || 5000;
 
 const products_routes = require("./routes/Products")
+
+
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("hi iam live raju bhai")
 });
 
-
-app.use(cors({origin:"http://localhost:5173",},"/api/products", products_routes) )
- 
+app.use("/api/products", products_routes) 
  
 const Start = async () => {
     try{

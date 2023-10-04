799d15b3c193e148827bb25ae9d0211c16f95638
Harshchaprana <123630469+Harshchaprana@users.noreply.github.com>
2023-10-04 18:16:54 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,15 +1,38 @@
 const mongoose = require("mongoose");
 
-
 const productSchema = new mongoose.Schema({
+    
+    
+
+    image: {
+        type: String, // Use String type for storing the image URL
+        required: true // Add other options as needed
+      },
+
     name: {
         type: String,
         required: true,
+        
     },
+
+    description : {
+        type: String,
+        required: true,
+        default:"The radiance lives on in the Air Force 1 '07 LV8. This b-ball original puts a fresh spin on what you know best: durably stitched overlays, clean finishes and the perfect amount of flash to make you shine. This winter-ready version helps keep you warm and has traction to beat the elements.",
+        
+    },
+
+     
     price:{
         type:Number,
         required: [true,"price must be provided"],
     },
+    stars:{
+        type:Number,
+        required:true,
+        default:3.5,
+    },
+
     featured: {
         type: Boolean,
         default:false,
@@ -29,6 +52,9 @@ const productSchema = new mongoose.Schema({
             message:`{VALUE} is not supported` ,
         }
     },
+    
 });
 
-module.exports = mongoose.model("Product", productSchema);
+const Nikeshoue = new mongoose.model("Nikeshoues", productSchema);
+
+ module.exports = Nikeshoue

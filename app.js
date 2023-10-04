f00be76b803bc3bb53d8fff0c376b7ee3ea961e4
Harshchaprana <123630469+Harshchaprana@users.noreply.github.com>
2023-10-04 18:17:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const connectDB = require("./db/connect");
+require("./db/connect");
 const cors = require("cors")
 const PORT = process.env.PORT || 5000;
-
-const products_routes = require("./routes/Products")
+const router = require("./routes/Products")
 
 
 app.use(cors());
-
-
+app.use(express.json());
+app.use(router);
 app.get("/", (req, res) => {
   res.send("hi iam live raju bhai")
 });
 
-app.use("/api/products", products_routes) 
+// app.use("/products", router) 
  
-const Start = async () => {
-    try{
-        await connectDB(process.env.MONGODB_URL);
-        app.listen(PORT , () => {
-         console.log(`${PORT} Yes I am connected raju bhai`)   
-        }) 
-    } catch (error) {
-        console.log(error)
-    }
-}
+// const Start = async () => {
+//     try{
+//         await connectDB(process.env.MONGODB_URL);
+//         app.listen(PORT , () => {
+//          console.log(`${PORT} Yes I am connected raju bhai`)   
+//         }) 
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-Start();
+// Start();
+app.listen(PORT , () => {
+    console.log(`${PORT} Yes I am connected raju bhai`)   
+   }) 

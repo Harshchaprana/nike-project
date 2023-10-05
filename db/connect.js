faad8413be30d7519534df457cdff6d96569b6d8
Harshchaprana <123630469+Harshchaprana@users.noreply.github.com>
2023-10-05 17:37:42 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,27 +1,20 @@
-// const mongoose = require("mongoose");
-
-
-// const connectDB = (uri) => {
-    
-//     return mongoose.connect(uri,{
-//         useNewUrlParser: true,
-//         useUniFiedTopology:true,
-//     });
-// };
-
-
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 
- 
-mongoose
-   .connect("mongodb://127.0.0.1:27017/nike",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(() => {
-     console.log("connection succes");
-}).catch((e) => {
-    console.log("connection fail");
-})
+
+const connectDB = (uri) => {
+    
+    return mongoose
+    .connect(uri,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        }).then(() => {
+             console.log("connection succes");
+        }).catch((e) => {
+            console.log("connection fail");
+        })
+        
+};
+
+
+
+module.exports = connectDB;
